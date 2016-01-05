1509a03ceecef53b72e17be05762a7656bd5fb20
Lindsey Ma <lma2@stuy.edu>
2016-01-04 21:39:00 -0500

    Started sorts!

---
+++ hwBarCode/Sorts.java
@@ -0,0 +1,15 @@
+public class Sorts{
+
+    static void selectionSort(){
+	for (int i=0; i<data.length; i++){
+	    for(int j=0; j<data.length; j++){
+		int x=data[j];//current value
+		    if(data[j]<x){
+			x=data[j];
+		    }//if
+		data[i]=x;
+	    }//for
+	}//for
+    }//selection
+	    
+}//class
